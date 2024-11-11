0789ef6b429ea01f5f718f57c371a280c78a46cb
nataraz <natarazjavaarena@gmail.com>
2024-11-11 10:12:07 +0530

    TL --Branch2  enhacement

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
@@ -8,4 +8,13 @@ public class NetBankingService {
 	public   String openAccount(String addhar, double initialamount, String type) {
 		return "account is opened with acc number::"+new Random().nextLong(500000000000L);
 	}
+	
+	public  String withdraw(long acno, double amount) {
+		return  amount+" is withdrawn from  the account number::"+acno;
+	}
+	
+	public  String deposite(long acno, double amount) {
+		return  amount+" is deposited in to  the account number::"+acno;
+	}
+	
 }
