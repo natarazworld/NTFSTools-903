dd83c0b825019cff17d949eb6f097972d68a3544
nataraz <natarazjavaarena@gmail.com>
2024-11-11 10:31:32 +0530

    Dev1-- close accoint

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
@@ -8,4 +8,10 @@ public class NetBankingService {
 	public   String openAccount(String addhar, double initialamount, String type) {
 		return "account is opened with acc number::"+new Random().nextLong(500000000000L);
 	}
+	
+	public   String closeAccount() {
+		return "Account is closed";
+	}
+	
+	
 }
