e3f022c29912e717232d4da70bb0fc851460bffc
nataraz <natarazjavaarena@gmail.com>
2024-11-11 10:09:37 +0530

    dev1 --- account opening logic

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
@@ -1,5 +1,11 @@
 package com.nt.netbanking;
 
+import java.util.Random;
+
 public class NetBankingService {
 
+	
+	public   String openAccount(String addhar, double initialamount, String type) {
+		return "account is opened with acc number::"+new Random().nextLong(500000000000L);
+	}
 }
